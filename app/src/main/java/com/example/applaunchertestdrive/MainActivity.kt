4b5d8a866c7b1c4e4c50db1b5ebf15af2f134af6
meridith <mjunk@pillartechnology.com>
2019-03-13 12:57:15 -0500

    updated to have two tests and a new button

--- app/src/main/java/com/example/applaunchertestdrive/MainActivity.kt
+++ app/src/main/java/com/example/applaunchertestdrive/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.applaunchertestdrive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        chromeButton.setOnClickListener { onChromeButtonClick() }
+
+    }
+
+    fun onChromeButtonClick() {
+        val launchIntent = packageManager.getLaunchIntentForPackage("com.android.chrome")
+        startActivity(launchIntent)
     }
 }
